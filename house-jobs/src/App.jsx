d2891da9f806f6ee0363dd00c8de19e609c5b4e8
Mason Menosky <mmmenosk@mtu.edu>
2026-09-16 22:22:04 -0400

    try 2

--- house-jobs/src/App.jsx
+++ house-jobs/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo, useCallback, useRef } from "react";
 
 /*
- * HOUSE JOBS — Firebase Realtime Database Edition, adding new security features
+ * HOUSE JOBS — Firebase Realtime Database Edition
  * With Sunday Cleaning + House Projects
  */
 
